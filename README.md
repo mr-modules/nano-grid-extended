ecaf0a34326db0560cee1deb44e5f0bba5d8956d
Miguel <jemiguelrivas@gmail.com>
2022-05-31 13:20:37 -0400

    splitting nano grid

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nano-grid-extended
+# Nano Grid
+
+## Publish new version
+```bash
+yarn publish
+```
+
+### v2.1
+
+Better parsing for the formulas.
---
+++ components/index.js
@@ -0,0 +1,7 @@
+import ScrollArea from "./scroll-area";
+import Icon from "./icon";
+import Container from "./container";
+
+window.customElements.define('nn-icon', Icon);
+window.customElements.define('nn-scroll-area', ScrollArea);
+window.customElements.define('nn-container', Container);
